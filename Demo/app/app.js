55984824dcde4e57cfda379977b92e99c06405d8
Joseph Wunz <jodaman19@gmail.com>
2018-08-08 13:05:07 -0600

    Here's the baseline!

---
+++ Demo/app/app.js
@@ -0,0 +1,30 @@
+var stage = null;
+var mousePosition = {x:0, y:0};
+
+//Settings to be used throughout the program
+
+var SCREEN_WIDTH = 800;
+var SCREEN_HEIGHT = 600;
+
+//Function for initializing the canvas
+
+var setupCanvas = function() {
+    var canvas = document.getElementById('game');
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+
+    //The canvas will be our "stage" for our game "actors" to perform on
+
+    stage = new createjs.Stage(canvas);
+};
+
+//Initialize the app
+
+var init = function() {
+
+    //Set up the canvas
+
+    setupCanvas();
+
+    
+};
